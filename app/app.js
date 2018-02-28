44100677ae9f01454d5a849820b5bba82abfd873
Dinesh <flexdinesh@gmail.com>
2018-02-28 17:42:00 +0530

    Remove .htaccess file

--- app/app.js
+++ app/app.js
@@ -38,7 +38,6 @@ import '!file-loader?name=[name].[ext]!./images/icon-192x192.png';
 import '!file-loader?name=[name].[ext]!./images/icon-384x384.png';
 import '!file-loader?name=[name].[ext]!./images/icon-512x512.png';
 import '!file-loader?name=[name].[ext]!./manifest.json';
-import 'file-loader?name=[name].[ext]!./.htaccess'; // eslint-disable-line import/extensions
 /* eslint-enable import/no-webpack-loader-syntax */
 
 import configureStore from './configureStore';
