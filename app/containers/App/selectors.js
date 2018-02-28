a961f2d40502925ca49962c3b344f779b65732e1
Dinesh <flexdinesh@gmail.com>
2018-02-28 17:22:22 +0530

    Commit after init

---
+++ app/containers/App/selectors.js
@@ -0,0 +1,43 @@
+/**
+ * The global state selectors
+ */
+
+import { createSelector } from 'reselect';
+
+const selectGlobal = (state) => state.get('global');
+
+const selectRoute = (state) => state.get('route');
+
+const makeSelectCurrentUser = () => createSelector(
+  selectGlobal,
+  (globalState) => globalState.get('currentUser')
+);
+
+const makeSelectLoading = () => createSelector(
+  selectGlobal,
+  (globalState) => globalState.get('loading')
+);
+
+const makeSelectError = () => createSelector(
+  selectGlobal,
+  (globalState) => globalState.get('error')
+);
+
+const makeSelectRepos = () => createSelector(
+  selectGlobal,
+  (globalState) => globalState.getIn(['userData', 'repositories'])
+);
+
+const makeSelectLocation = () => createSelector(
+  selectRoute,
+  (routeState) => routeState.get('location').toJS()
+);
+
+export {
+  selectGlobal,
+  makeSelectCurrentUser,
+  makeSelectLoading,
+  makeSelectError,
+  makeSelectRepos,
+  makeSelectLocation,
+};
---
+++ server/index.js
@@ -0,0 +1,32 @@
+/* eslint consistent-return:0 */
+
+const express = require('express');
+const logger = require('./util//logger');
+
+const argv = require('./util/argv');
+const port = require('./util//port');
+const setup = require('./middlewares/frontendMiddleware');
+const resolve = require('path').resolve;
+const app = express();
+
+// If you need a backend, e.g. an API, add your custom backend-specific middleware here
+// app.use('/api', myApi);
+
+// In production we need to pass these values in instead of relying on webpack
+setup(app, {
+  outputPath: resolve(process.cwd(), 'build'),
+  publicPath: '/',
+});
+
+// get the intended host and port number, use localhost and port 3000 if not provided
+const customHost = argv.host || process.env.HOST;
+const host = customHost || null; // Let http.Server use its default IPv6/4 host
+const prettyHost = customHost || 'localhost';
+
+// Start your app.
+app.listen(port, host, (err) => {
+  if (err) {
+    return logger.error(err.message);
+  }
+  logger.appStarted(port, prettyHost);
+});
