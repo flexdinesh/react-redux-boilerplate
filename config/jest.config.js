eaadf50f7babea995cd6250104c3f1c3261dc6ad
Dinesh <flexdinesh@gmail.com>
2018-03-03 14:33:03 +0530

    Move jest config to its own file

---
+++ config/jest.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  collectCoverageFrom: [
+    'app/**/*.{js,jsx}',
+    '!app/**/*.test.{js,jsx}',
+    '!app/*/RbGenerated*/*.{js,jsx}',
+    '!app/app.js',
+    '!app/*/*/Loadable.{js,jsx}'
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 98,
+      branches: 91,
+      functions: 98,
+      lines: 98
+    }
+  },
+  coverageReporters: ['json', 'lcov', 'text-summary'],
+  moduleDirectories: ['node_modules', 'app'],
+  moduleNameMapper: {
+    '.*\\.(css|less|styl|scss|sass)$':
+      '<rootDir>/config/jest-mocks/cssModule.js',
+    '.*\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/config/jest-mocks/image.js'
+  },
+  setupTestFrameworkScriptFile: '<rootDir>/config/test-setup.js',
+  testRegex: 'tests/.*\\.test\\.js$'
+};
