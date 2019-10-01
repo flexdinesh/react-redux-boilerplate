0450e505d3d7c4e55ac5c185410941ae8815fdfd
Dinesh Pandiyan <flexdinesh@gmail.com>
2019-10-01 19:50:00 +1000

    Ignrore init from coverage

--- app/init.js
+++ app/init.js
@@ -1,9 +1,11 @@
 import FontFaceObserver from 'fontfaceobserver';
 
+/* istanbul ignore next */
 // Observe loading of Open Sans (to remove open sans, remove the <link> tag in
 // the index.html file and this observer)
 const openSansObserver = new FontFaceObserver('Open Sans', {});
 
+/* istanbul ignore next */
 // When Open Sans is loaded, add a font-family using Open Sans to the body
 const registerOpenSans = () => openSansObserver.load().then(() => {
   document.body.classList.add('fontLoaded');
